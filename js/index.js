d364b7f117eb47d88a9165eb1b81914c11997095
cullus <lst174523@gmail.com>
2016-09-09 10:35:45 +0800

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,20 @@
+function loadIndex(){
+	var jsonImage = [
+	{"name" : "", "src" : "image/bgCommon.png"},
+	{"name" : "", "src" : "image/bgIndexSide.png"},
+	{"name" : "", "src" : "image/bgIndexUp.png"},
+	{"name" : "", "src" : "image/iconIndex.png"},
+	{"name" : "", "src" : "image/iconIndexAR.png"},
+	{"name" : "", "src" : "image/iconIndexHG.png"},
+	{"name" : "", "src" : "image/iconIndexMG.png"},
+	{"name" : "", "src" : "image/iconIndexRF.png"},
+	{"name" : "", "src" : "image/iconIndexSMG.png"},
+	{"name" : "", "src" : "image/iconIndexPercent.png"},
+	{"name" : "", "src" : "image/iconIndexPercentUp.png"},
+	{"name" : "", "src" : "image/iconIndexEquipments.png"},
+	{"name" : "", "src" : "image/iconIndexEquipmentsNone.png"},
+	{"name" : "", "src" : "image/iconIndexTag.png"},
+	{"name" : "", "src" : "image/iconIndexStar.png"},
+	{"name" : "", "src" : "image/iconReturnToBase.png"}
+	];
+}
